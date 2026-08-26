2fea91065f98023b70c3c31ebe99bf80c4eb2935
andre <andreevaristotavares@outlook.com>
2026-08-26 09:06:53 -0300

    colocando uma interface html para testar

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require('cors');
 
 const tarefaRoutes = require("./routes/tarefaRoutes");
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 
 app.use("/tarefas", tarefaRoutes);
 
