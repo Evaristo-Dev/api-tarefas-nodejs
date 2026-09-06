727446d06ae9266189c3fb0485fbd37b0e967cc6
andre <andreevaristotavares@outlook.com>
2026-09-06 09:31:38 -0300

    query atualizada na inserção de tarefas model

--- src/models/tarefaModel.js
+++ src/models/tarefaModel.js
@@ -1,7 +1,7 @@
 const connection = require('../config/database');
 module.exports = {
     criar: function(titulo, descricao, callback) {
-        const query = 'INSERT INTO tarefas (titulo, descricao) VALUES (?, ?)';
+        const query = 'INSERT INTO tarefa (titulo, descricao) VALUES (?, ?)';
         connection.query(query, [titulo, descricao], (err, resultado) => {
             if (err) {
                 console.error('Erro ao criar tarefa no banco de dados:', err);
@@ -11,7 +11,7 @@ module.exports = {
         });
     },
     listar: function(callback) {
-        const query = 'SELECT * FROM tarefas';
+        const query = 'SELECT * FROM tarefa';
         connection.query(query, (err, resultados) => {
             if (err) {
                 console.error('Erro ao listar tarefas no banco de dados:', err);
@@ -21,7 +21,7 @@ module.exports = {
         });
     },
     atualizar: function(id, titulo, descricao, callback) {
-        const query = 'UPDATE tarefas SET titulo = ?, descricao = ? WHERE id = ?';
+        const query = 'UPDATE tarefa SET titulo = ?, descricao = ? WHERE id = ?';
         connection.query(query, [titulo, descricao, id], (err, resultado) => {
             if (err) {
                 console.error('Erro ao atualizar tarefa no banco de dados:', err);
@@ -31,7 +31,7 @@ module.exports = {
         });
     },
     deletar: function(id, callback) {
-        const query = 'DELETE FROM tarefas WHERE id = ?';
+        const query = 'DELETE FROM tarefa WHERE id = ?';
         connection.query(query, [id], (err, resultado) => {
             if (err) {
                 console.error('Erro ao deletar tarefa no banco de dados:', err);
